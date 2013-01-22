0e985e554221884ba8d94d05faa02707b6d5bc13
Mike <mike@mike-H55-UD3H.(none)>
2013-01-22 10:40:01 +0000

    center 3.6 with old appearance panel

---
+++ config.h
@@ -0,0 +1,111 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Gettext package */
+#define GETTEXT_PACKAGE "gnome-control-center-2.0"
+
+/* Enable additional debugging at the expense of performance and size */
+/* #undef GNOME_ENABLE_DEBUG */
+
+/* Define to 1 to enable cheese webcam support */
+/* #undef HAVE_CHEESE */
+
+/* Define to 1 if you have the <cups/cups.h> header file. */
+#define HAVE_CUPS_CUPS_H 1
+
+/* Define to 1 if you have the <cups/http.h> header file. */
+#define HAVE_CUPS_HTTP_H 1
+
+/* Define to 1 if you have the <cups/ipp.h> header file. */
+#define HAVE_CUPS_IPP_H 1
+
+/* Define to 1 if you have the <cups/ppd.h> header file. */
+#define HAVE_CUPS_PPD_H 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Defined if libsocialweb is available */
+/* #undef HAVE_LIBSOCIALWEB */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if systemd is available */
+/* #undef HAVE_SYSTEMD */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86misc.h> header file. */
+/* #undef HAVE_X11_EXTENSIONS_XF86MISC_H */
+
+/* Define to 1 if you have the <X11/extensions/XKB.h> header file. */
+#define HAVE_X11_EXTENSIONS_XKB_H 1
+
+/* Define to 1 if you have the <X11/Xlib.h> header file. */
+#define HAVE_X11_XLIB_H 1
+
+/* ISO codes prefix */
+#define ISO_CODES_PREFIX "/usr"
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "gnome-control-center"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://bugzilla.gnome.org/enter_bug.cgi?product=gnome-control-center"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gnome-control-center"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gnome-control-center 3.6.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gnome-control-center"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.6.3"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "3.6.3"
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
